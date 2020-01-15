e7d58e629bf11392c28fd326bca9fc16e143b50e
filippinheiro <filippinheiro1@gmail.com>
2020-01-15 15:28:22 -0300

    feature: aula 03

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,23 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const routes = require('./src/routes')
+const cors = require('cors')
 
 dotenv.config()
+
+
 const app = express()
-mongoose.connect(process.env.CONNECTION_STRING, {
+
+const {PORT, CONNECTION_STRING} = process.env
+
+mongoose.connect(CONNECTION_STRING, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
    useCreateIndex: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes) 
 
-app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}`)) 
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`)) 
